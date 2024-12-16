903c847aeca1181167beff90ae4146dc8c4a13cf
csphat001 <csphat1@gmail.com>
2024-12-16 15:01:29 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+const ReactCompilerConfig = {
+  target: '19',
+};
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+  ],
+});
