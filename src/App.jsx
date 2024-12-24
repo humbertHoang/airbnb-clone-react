1ad57889073e5ceca2aa82a26dc9dcad8e8e0ec5
csphat001 <csphat1@gmail.com>
2024-12-24 16:42:24 +0700

    add admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
-import ClientTemplate from "./pages/templates/ClientTemplate";
-import { ToastContainer } from "react-toastify";
-import Page404 from "./pages/Page404";
-import DetailRoomPage from "./pages/DetailRoomPage";
-import LoginComponent from "./components/LoginComponent";
-import SignUpComponent from "./components/Singupcomponent";
-
-
-
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { ToastContainer } from 'react-toastify';
+import LoginComponent from './components/LoginComponent';
+import SignUpComponent from './components/Singupcomponent';
+import DashboardPage from './pages/admin/DashboardPage';
+import DetailRoomPage from './pages/DetailRoomPage';
+import HomePage from './pages/HomePage';
+import Page404 from './pages/Page404';
+import AdminTemplate from './pages/templates/AdminTemplate';
+import ClientTemplate from './pages/templates/ClientTemplate';
 
 const App = () => {
-  
-
   return (
     <BrowserRouter>
       <Routes>
+        {/* Client Routes */}
         <Route path="/" element={<ClientTemplate />}>
           <Route path="" element={<HomePage />} />
           <Route path="room/:id" element={<DetailRoomPage />} />
           <Route path="login" element={<LoginComponent />} />
           <Route path="register" element={<SignUpComponent />} />
-          <Route path="*" element={<Page404 />} />
-          
         </Route>
+
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminTemplate />}>
+          <Route path="" element={<DashboardPage />} />
+          <Route path="rooms" element={<div>Rooms Management</div>} />
+          <Route path="users" element={<div>Users Management</div>} />
+          <Route path="bookings" element={<div>Bookings Management</div>} />
+        </Route>
+
+        {/* 404 Route */}
+        <Route path="*" element={<Page404 />} />
       </Routes>
       <ToastContainer autoClose={1000} />
     </BrowserRouter>
